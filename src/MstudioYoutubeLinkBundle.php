7960cc7899bbe4a1016666e603d978af9c3de5cf
Markus Schnagl <mail@mstudio.de>
2025-12-25 09:51:19 +0100

    Fix DCA loading and add bundle dependencies

--- src/MstudioYoutubeLinkBundle.php
+++ src/MstudioYoutubeLinkBundle.php
@@ -6,4 +6,5 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MstudioYoutubeLinkBundle extends Bundle
 {
+    public function getBundleDependencies(): array { return [ ContaoCoreBundle::class ]; }
 }
